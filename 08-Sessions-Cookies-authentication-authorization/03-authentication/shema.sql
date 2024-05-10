7a385d5b420b8731d7ad3705c3da4f51dbed49e1
nizar-maatoug <nizar.maatoug@hotmail.com>
2024-05-10 16:14:40 +0100

    v3

---
+++ 08-Sessions-Cookies-authentication-authorization/03-authentication/shema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS demo_authorization_db;
+USE demo_authorization_db;
+
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(60) NOT NULL,
+    password VARCHAR(255) NOT NULL,    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (username, password) VALUES ('admin', '$2y$10$YmZJUPCtlvfqmrn6xAZlZOgWjZPnKXmkhP/fBZVscpxTgQzJ2vwTu', 'ADMIN');
+INSERT INTO users (username, password) VALUES ('user', '$2y$10$suUp8bAj5H6IA8EoUaiS4OEgGjO4AHQWb/ao7Ac//.NVmWICg/0Va', 'USER');
+
