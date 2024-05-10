4f114f36adbfccbda48e943f1f4c7985499fda3f
nizar-maatoug <nizar.maatoug@hotmail.com>
2024-05-10 14:29:30 +0100

    v2

--- 07-database/10-insert-record/create.php
+++ 07-database/10-insert-record/create.php
@@ -44,14 +44,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
   <div class="flex justify-center mt-10">
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
       <h1 class="text-2xl font-semibold mb-6">Create Blog Post</h1>
-      <form method="post">
+      <form method="post" action="create.php">
         <div class="mb-4">
           <label for="title" class="block text-gray-700 font-medium">Title</label>
-          <input type="text" id="title" name="title" placeholder="Enter post title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none" value="<?= $title ?>">
+          <input type="text" id="title" name="title" placeholder="Enter post title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none" >
         </div>
         <div class="mb-6">
           <label for="body" class="block text-gray-700 font-medium">Body</label>
-          <textarea id="body" name="body" placeholder="Enter post body" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"><?= $body ?></textarea>
+          <textarea id="body" name="body" placeholder="Enter post body" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"></textarea>
         </div>
         <div class="flex items-center justify-between">
           <button type="submit" name="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none">
